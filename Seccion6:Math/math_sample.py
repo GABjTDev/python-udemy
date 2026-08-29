69958e3992c6c07b0b82a281234333469ce06b61
GABjTDev <47054908+GABjTDev@users.noreply.github.com>
2026-08-29 15:04:53 -0300

    Add math and random sample scripts demonstrating various mathematical operations and random value generation

---
+++ Seccion6:Math/math_sample.py
@@ -0,0 +1,58 @@
+
+import math
+
+absolute = abs(-5)
+print(absolute)
+
+absolute = abs(5)
+print(absolute)
+
+maximum = max(3.5, 5)
+print(maximum)
+
+minimum = min(3.5, 3.7, 1.2, 7, 4.8)
+print(minimum)
+
+ceiling = math.ceil(3.00001)
+print(ceiling)
+
+floor = math.floor(3.9)
+print(floor)
+
+rounded_number = round(math.pi)
+print(rounded_number)
+print(math.pi)
+
+print(math.e)
+exponential = math.exp(1)
+print(exponential)
+
+exponential = math.exp(3)
+print(exponential)
+print(math.e * math.e * math.e)
+print(math.pow(math.e, 3))
+
+power = math.pow(10, 3)
+print(power)
+
+square_root = math.sqrt(5)
+print(square_root)
+
+degrees = math.degrees(1.57)
+print(degrees)
+
+radians = math.radians(90.00)
+print(radians)
+
+print(math.sin(radians))
+print(math.cos(radians))
+
+radians = math.radians(180.00)
+print(radians)
+print(math.sin(radians))
+print(math.cos(radians))
+
+radians = math.radians(0.00)
+print(radians)
+print(math.sin(radians))
+print(math.cos(radians))
---
+++ Seccion6:Math/random_sample.py
@@ -0,0 +1,24 @@
+import math
+import random
+
+colors = ['azul', 'amarillo', 'rojo', 'verde', 'blanco', 'negro']
+random_val = random.random()
+random_val *= len(colors)
+random_val = math.floor(random_val)
+print(f'color: {colors[int(random_val)]}')
+print('Numero aleatorio: ', random_val)
+
+random_int = random.randint(15, 25)
+print(random_int)
+
+random_val = random.randint(0, len(colors) - 1)
+print(colors[random_val])
+
+random_range = random.randrange(len(colors))
+print(colors[random_range])
+
+random_range = random.randrange(2, 9, 3)
+print(random_range)
+
+random_value = random.uniform(15, 25)
+print(random_value)
